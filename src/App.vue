d72e31c6547b101fd102e6df0e4e1f5b362ba83b
HungEplus <truonghung239810@gmail.com>
2023-01-31 17:32:55 +0700

    _: firebase analytic

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,28 @@ import Projects from "./components/Projects.vue";
 import Footer from "./components/Footer.vue";
 
 import { bucket } from "./cosmic.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDZ8gYDFJMGlGER8j3TrJJzMrtHXmYFec",
+  authDomain: "portfolio-3c638.firebaseapp.com",
+  projectId: "portfolio-3c638",
+  storageBucket: "portfolio-3c638.appspot.com",
+  messagingSenderId: "793670588427",
+  appId: "1:793670588427:web:aeccf8243772fae2850f0e",
+  measurementId: "G-C1GFGF8CCF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
 export default {
   name: "App",
   components: {
